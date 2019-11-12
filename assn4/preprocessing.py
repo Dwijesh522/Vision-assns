7434c516248528dbe0a024afc3b7acbf03b0b6ee
dwijesh522 <iddwijesh@gmail.com>
2019-11-12 20:10:57 +0530

    added a function to automatically create dataset from videos given some params

--- assn4/preprocessing.py
+++ assn4/preprocessing.py
@@ -103,7 +103,7 @@ def train_network(net, custom_dataset):
     # optimizer = optim.Adadelta(net.parameters(), lr=0.01)
     mini_batch = 1405
     loss_values = []
-    for epoch in range(10):  # loop over the dataset multiple times
+    for epoch in range(1):  # loop over the dataset multiple times
         running_loss = 0.0
         # rand_num = random.randrange(30,100,3)        
         for i, data in enumerate(custom_dataset, 0):
@@ -142,6 +142,119 @@ def train_network(net, custom_dataset):
     # print(loss_values)
     print('Finished Training...')
 
+# given a path to videos extract the frames and apply the preprocessing to the images and store it in a given directory
+def store_dataset_from_video(path, csv_file_name,store_it_here, one_of_how_many):
+    stop_path = path + "stop/"
+    next_path = path + "next/"
+    prev_path = path + "prev/"
+    
+    stop_video_names = [f for f in listdir(stop_path) if isfile(join(stop_path, f))]
+    prev_video_names = [f for f in listdir(prev_path) if isfile(join(prev_path, f))]
+    next_video_names = [f for f in listdir(next_path) if isfile(join(next_path, f))]
+
+    # changing the dir
+    if(os.path.isdir(store_it_here)):
+        shutil.rmtree(store_it_here)
+
+    os.mkdir(store_it_here)
+    os.chdir(store_it_here)
+
+    # deleting csv file if already exists
+    if(os.path.exists("unordered_dataset.csv")):
+        os.remove("unordered_dataset.csv")
+
+    # printing header row in csv file
+    # printing into csv file
+    row = ["image_names", "label"]
+    with open('unordered_dataset.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(row)
+
+    # iterating over sstop video frames
+    i=0
+    image_count = 0
+    while(i < len(stop_video_names)):
+        cap = cv2.VideoCapture(stop_path + stop_video_names[i])
+        fgbg = cv2.createBackgroundSubtractorKNN(5000, 400, False)
+        ret = 1
+        while(ret):
+            ret, frame = cap.read()
+            if(not ret): break
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            canny_frame = cv2.Canny(frame,20, 50, apertureSize = 3)
+            canny_frame = cv2.resize(canny_frame, (50, 50), interpolation = cv2.INTER_AREA)
+            if(image_count%one_of_how_many == 0):
+                rand1 = random.randint(0, 1000000)
+                rand2 = random.randint(0, 1000000)
+                cv2.imwrite(str(rand1)+ "_" + str(rand2) + ".jpg", canny_frame)
+                # printing into csv file
+                row = [str(rand1)+ "_" + str(rand2) + ".jpg", str(1)]
+                # cv2.imwrite("stop_" + str(image_count) + ".jpg", canny_frame)
+                with open('unordered_dataset.csv', 'a') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerow(row)
+            image_count += 1
+        i += 1
+    # iterating over next video frames
+    i=0
+    image_count = 0
+    while(i < len(next_video_names)):
+        cap = cv2.VideoCapture(next_path + next_video_names[i])
+        fgbg = cv2.createBackgroundSubtractorKNN(5000, 400, False)
+        ret = 1
+        while(ret):
+            ret, frame = cap.read()
+            if(not ret): break
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            canny_frame = cv2.Canny(frame,20, 50, apertureSize = 3)
+            canny_frame = cv2.resize(canny_frame, (50, 50), interpolation = cv2.INTER_AREA)
+            if(image_count%one_of_how_many == 0):
+                rand1 = random.randint(0, 1000000)
+                rand2 = random.randint(0, 1000000)
+                cv2.imwrite(str(rand1)+ "_" + str(rand2) + ".jpg", canny_frame)
+                # printing into csv file
+                row = [str(rand1)+ "_" + str(rand2) + ".jpg", str(2)]
+                with open('unordered_dataset.csv', 'a') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerow(row)
+            image_count += 1
+        i += 1
+    # iterating over prev video frames
+    i=0
+    image_count = 0
+    while(i < len(prev_video_names)):
+        cap = cv2.VideoCapture(prev_path + prev_video_names[i])
+        fgbg = cv2.createBackgroundSubtractorKNN()
+        ret = 1
+        while(ret):
+            ret, frame = cap.read()
+            if(not ret): break
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            canny_frame = cv2.Canny(frame,20, 50, apertureSize = 3)
+            canny_frame = cv2.resize(canny_frame, (50, 50), interpolation = cv2.INTER_AREA)
+            if(image_count%one_of_how_many == 0):
+                rand1 = random.randint(0, 1000000)
+                rand2 = random.randint(0, 1000000)
+                cv2.imwrite(str(rand1)+ "_" + str(rand2) + ".jpg", canny_frame)
+                # printing into csv file
+                row = [str(rand1)+ "_" + str(rand2) + ".jpg", str(0)]
+                # cv2.imwrite("prev_" + str(image_count) + ".jpg", canny_frame)
+                # printing into csv file
+                # row = ["prev_" + str(image_count) + ".jpg", str(0)]
+                with open('unordered_dataset.csv', 'a') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerow(row)
+            image_count += 1
+        i += 1
+    #shuffling the csv file
+
+    df = pd.read_csv('unordered_dataset.csv')
+    df = df.sample(frac = 1).reset_index(drop = True)
+
+    export_csv = df.to_csv(csv_file_name, index = None, header = True)
+    os.remove("unordered_dataset.csv")
+    # coming out of dir
+    os.chdir("..")
 
 
 if __name__ == '__main__':
@@ -149,116 +262,7 @@ if __name__ == '__main__':
     # getting the path to three types of videos
     # print("\ndefault path: /home/dwijesh/Documents/sem5/vision/assns/assn4_data/videos/\n")
     # path = input("Enter the path to stop, prev, next folders: ")
-    # stop_path = path + "stop/"
-    # next_path = path + "next/"
-    # prev_path = path + "prev/"
-    
-    # stop_video_names = [f for f in listdir(stop_path) if isfile(join(stop_path, f))]
-    # prev_video_names = [f for f in listdir(prev_path) if isfile(join(prev_path, f))]
-    # next_video_names = [f for f in listdir(next_path) if isfile(join(next_path, f))]
-
-    # # changing the dir
-    # if(os.path.isdir("train_dataset")):
-    #     shutil.rmtree("train_dataset")
-
-    # os.mkdir("train_dataset")
-    # os.chdir("train_dataset")
-
-    # # deleting csv file if already exists
-    # if(os.path.exists("training_dataset.csv")):
-    #     os.remove("training_dataset.csv")
-
-    # # printing header row in csv file
-    # # printing into csv file
-    # row = ["image_names", "label"]
-    # with open('training_dataset.csv', 'a') as csvFile:
-    #     writer = csv.writer(csvFile)
-    #     writer.writerow(row)
-
-    # # iterating over sstop video frames
-    # i=0
-    # image_count = 0
-    # while(i < len(stop_video_names)):
-    #     cap = cv2.VideoCapture(stop_path + stop_video_names[i])
-    #     fgbg = cv2.createBackgroundSubtractorKNN(5000, 400, False)
-    #     ret = 1
-    #     while(ret):
-    #         ret, frame = cap.read()
-    #         if(not ret): break
-    #         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #         canny_frame = cv2.Canny(frame,20, 50, apertureSize = 3)
-    #         canny_frame = cv2.resize(canny_frame, (50, 50), interpolation = cv2.INTER_AREA)
-    #         if(image_count%15 == 0):
-    #             rand1 = random.randint(0, 1000000)
-    #             rand2 = random.randint(0, 1000000)
-    #             cv2.imwrite(str(rand1)+ "_" + str(rand2) + ".jpg", canny_frame)
-    #             # printing into csv file
-    #             row = [str(rand1)+ "_" + str(rand2) + ".jpg", str(1)]
-    #             # cv2.imwrite("stop_" + str(image_count) + ".jpg", canny_frame)
-    #             with open('training_dataset.csv', 'a') as csvFile:
-    #                 writer = csv.writer(csvFile)
-    #                 writer.writerow(row)
-    #         image_count += 1
-    #     i += 1
-    # # iterating over next video frames
-    # i=0
-    # image_count = 0
-    # while(i < len(next_video_names)):
-    #     cap = cv2.VideoCapture(next_path + next_video_names[i])
-    #     fgbg = cv2.createBackgroundSubtractorKNN(5000, 400, False)
-    #     ret = 1
-    #     while(ret):
-    #         ret, frame = cap.read()
-    #         if(not ret): break
-    #         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #         canny_frame = cv2.Canny(frame,20, 50, apertureSize = 3)
-    #         canny_frame = cv2.resize(canny_frame, (50, 50), interpolation = cv2.INTER_AREA)
-    #         if(image_count%15 == 0):
-    #             rand1 = random.randint(0, 1000000)
-    #             rand2 = random.randint(0, 1000000)
-    #             cv2.imwrite(str(rand1)+ "_" + str(rand2) + ".jpg", canny_frame)
-    #             # printing into csv file
-    #             row = [str(rand1)+ "_" + str(rand2) + ".jpg", str(2)]
-    #             with open('training_dataset.csv', 'a') as csvFile:
-    #                 writer = csv.writer(csvFile)
-    #                 writer.writerow(row)
-    #         image_count += 1
-    #     i += 1
-    # # iterating over prev video frames
-    # i=0
-    # image_count = 0
-    # while(i < len(prev_video_names)):
-    #     cap = cv2.VideoCapture(prev_path + prev_video_names[i])
-    #     fgbg = cv2.createBackgroundSubtractorKNN()
-    #     ret = 1
-    #     while(ret):
-    #         ret, frame = cap.read()
-    #         if(not ret): break
-    #         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #         canny_frame = cv2.Canny(frame,20, 50, apertureSize = 3)
-    #         canny_frame = cv2.resize(canny_frame, (50, 50), interpolation = cv2.INTER_AREA)
-    #         if(image_count%15 == 0):
-    #             rand1 = random.randint(0, 1000000)
-    #             rand2 = random.randint(0, 1000000)
-    #             cv2.imwrite(str(rand1)+ "_" + str(rand2) + ".jpg", canny_frame)
-    #             # printing into csv file
-    #             row = [str(rand1)+ "_" + str(rand2) + ".jpg", str(0)]
-    #             # cv2.imwrite("prev_" + str(image_count) + ".jpg", canny_frame)
-    #             # printing into csv file
-    #             # row = ["prev_" + str(image_count) + ".jpg", str(0)]
-    #             with open('training_dataset.csv', 'a') as csvFile:
-    #                 writer = csv.writer(csvFile)
-    #                 writer.writerow(row)
-    #         image_count += 1
-    #     i += 1
-    # #shuffling the csv file
-
-    # df = pd.read_csv('training_dataset.csv')
-    # df = df.sample(frac = 1).reset_index(drop = True)
-
-    # export_csv = df.to_csv(r'train_dataset.csv', index = None, header = True)
-    # # coming out of dir
-    # os.chdir("..")
+    # store_dataset_from_video(path, "train_dataset.csv", "train_dataset", 15)
     transform = transforms.Compose([transforms.ToTensor(),
                               transforms.Normalize((0.5,), (0.5,)),
                               ])
@@ -294,3 +298,12 @@ if __name__ == '__main__':
             correct += (predicted_value == label_val).sum().item()
 
     print('Accuracy of the network on the train images: %d %%' % (100 * correct / total))
+
+    # adding the test dataset
+    print("\ndefault path: /home/dwijesh/Documents/sem5/vision/assns/assn4_data/test_videos/\n")
+    test_dataset_path = input("Enter the path to test_dataset folders: ")
+    store_dataset_from_video(test_dataset_path, "test_dataset.csv", "test_dataset", 20)
+    custom_dataset = customDataset(csv_file='test_dataset/test_dataset.csv', 
+                                    root_dir='/home/dwijesh/Documents/sem5/vision/assns/Vision-assns/assn4/test_dataset/')
+    print(custom_dataset)
+    
